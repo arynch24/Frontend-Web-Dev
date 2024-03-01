21e96e2b0510a4d03b8dc467aa46f7ed3532e57f
arynch24 <arynch24@gmail.com>
2024-03-01 19:32:49 +0530

    array done

---
+++ Javascript/JavaScript Prog/Arrays/practice_3.js
@@ -0,0 +1,7 @@
+let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+companies.shift();
+console.log(companies);
+companies.splice(1,1,"Ola");
+console.log(companies);
+companies.push("Amazon");
+console.log(companies);
