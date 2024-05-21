ee8685969d52180179de9da55aefa5cde192ed4e
arynch24 <arynch24@gmail.com>
2024-05-21 23:52:43 +0530

    datatypes

---
+++ Javascript/JS - Anurag Sir/Lect-1/script.js
@@ -0,0 +1,39 @@
+//variables
+var name1="ARYAN CHAUHAN";
+let name2 ="nothing";
+const name3="pwioi";
+
+console.log(name3);
+
+//datatypes
+//number-int ,float, doublr
+//string- char or combination of char
+//boolean - true or false
+//undefined- when the variable is declared but value is not assigned
+
+let loan_num=null;
+
+//array - [] , key is indexed
+//object - {} , key is of my choice
+
+//operstors - 
+//assignment operator
+//arithmetic operators - + * / %
+//logical && ||
+//relational  => > < .......................
+
+// Conditions -
+//if -> else if -> else
+
+//switch
+switch(name1){
+    case 1: {
+        break;
+    }
+}
+
+//ternary operrator
+//cond?true;false ->login
+
+______________________________________________________________
+
