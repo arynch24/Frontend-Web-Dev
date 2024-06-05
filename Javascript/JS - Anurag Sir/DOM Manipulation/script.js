a1efb72823368ffc4d7c2bd8beecf29ff61ad204
arynch24 <arynch24@gmail.com>
2024-06-05 19:43:22 +0530

    DOM

---
+++ Javascript/JS - Anurag Sir/DOM Manipulation/script.js
@@ -0,0 +1,19 @@
+// const var1= document.getElementsByTagName("h1");
+// console.log(var1);
+
+// const var2=document.getElementById("js1");
+// console.log(var2);
+// var2.outerText="Namaste";
+
+
+// const title=document.querySelectorAll("h1")
+// console.log(title);
+// title[3].className="FOURTH H1";
+
+
+
+
+// const btn =document.querySelector("button");
+// btn.addEventListener("click",function normal{
+// document.getElementById("click").innerHTML=
+// });
