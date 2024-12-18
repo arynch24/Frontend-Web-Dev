f0767342c06bf9c781d6d685ebb9eda0a3e040d3
arynch24 <arynch24@gmail.com>
2024-12-18 13:49:48 +0530

    react changes

---
+++ React/practice/Components/App.jsx
@@ -0,0 +1,10 @@
+import myImage from './assets/myimage.jpeg'
+
+export default function App() {
+  return (
+    <div> 
+        <h1>I have not done anything</h1>
+        <p>Take thenga!!</p>
+    </div>
+  )
+}
--- React/react-project-one/src/App.jsx
+++ React/react-project-one/src/App.jsx
@@ -1,26 +1,26 @@
-import UserCard from './components/user_card'
-import myImage from './assets/myimage.jpeg'
-import Portfolio from './components/portfolio'
-import './App.css'
+import React, { useState } from 'react';
+import UserCard from './components/user_card';
+import myImage from './assets/myimage.jpeg';
+import Portfolio from './components/portfolio';
+import './App.css';
+
+function Button({ onSmash, children }) {
+  return (
+    <button onClick={onSmash}>
+      {children}
+    </button>
+  );
+}
 
 export default function App() {
   return (
-    <div className="container">
-
-
-      <Portfolio>
-        <UserCard
-          name="Aryan Chauhan"
-          desc="I am invisible"
-          image={myImage}
-        />
-        <h1>I have not done anything</h1>
-        <p>Take thenga!!</p>
-      </Portfolio>
-
-
+    <div>
+      <Button onSmash={() => alert('Playing!')}>
+        Play Movie
+      </Button>
+      <Button onSmash={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
     </div>
-  )
+  );
 }
-
-
